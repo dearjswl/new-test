d62d6914e8f152f813bc6b51beb679ee14f65297
dearjswl <1251808313@qq.com>
2018-09-10 01:04:19 +0800

    qwer

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace new_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("this is my first c# program");
+        }
+    }
+}
